c908403baf5aa368ea837e0d0851005aa0703864
Thomas <ashernor@gmail.com>
2018-10-15 23:24:44 +0200

    Added a bit of css to improve the style and only take the last 5 surveys

--- pithermo/routes/index.js
+++ pithermo/routes/index.js
@@ -9,8 +9,10 @@ router.get('/', function(req, res, next) {
     if(err){
       console.log(err);
     } else{
-      let temperatures = _.meanBy(surveys, 'temperature');
-      console.log('retrieved list of names', temperatures);
+      // By taking the last 5 surveys we make an average on th last 10 minutes
+      let temperatures = _.meanBy(_.takeRight(surveys, 5), 'temperature');
+      // No we can order the dagta to to make a nice graph
+      // Survey by day
       res.render('index', { title: 'Temperature dans le salon', avgTemperature: temperatures});
     }
   });
